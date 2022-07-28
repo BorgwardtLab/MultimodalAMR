39a1ff6bb7c00e161bbb5ee890b97f222928adce
gvisona <visona.giovanni@gmail.com>
2022-07-28 15:29:49 +0200

    added random train val test split

--- data_split/data_utils.py
+++ data_split/data_utils.py
@@ -40,6 +40,23 @@ class DataSplitter():
     
     def __getitem__(self, idx):
         return self.long_table.iloc[idx]
+
+
+    ###############################################################
+    # SIMPLE DATA SPLIT FUNCTIONS
+
+    def random_train_val_test_split(self, val_size=0.1, test_size=0.2, random_state=42):
+        n_test = test_size if isinstance(test_size, int) else int(test_size*len(self.dataset))
+        n_val = val_size if isinstance(test_size, int) else int(test_size*len(self.dataset))
+        assert n_val+n_test < len(self.dataset), "Invalid val and test size"
+
+        train_val_X, test_X = train_test_split(self.dataset, stratify=self.dataset["response"], test_size=n_test, random_state=random_state)
+        train_X, val_X = train_test_split(train_val_X, stratify=train_val_X["response"], test_size=n_val, random_state=random_state**2)
+        return train_X, val_X, test_X
+
+    # END - SIMPLE DATA SPLIT FUNCTIONS
+    ###############################################################
+
     
     ###############################################################
     # BASELINE COMPARISON FUNCTIONS
